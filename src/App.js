ef0e92f6701706fdf47a52e8986e51790abcf785
Julian Nicholls <juliannicholls29@gmail.com>
2018-03-03 21:52:26 +0000

    fix info table

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import InputBox from './components/InputBox';
 import ColourInfo from './components/ColourInfo';
+import ColourBlocks from './components/ColourBlocks';
 
 class App extends React.Component {
   state = {
-    rgbs: ["#336699", "#336699", "#336699", "#336699", "#336699"],
+    rgbs: ["#336699", "#669933", "#996633", "#663399", "#339966"],
     hsls: [
       { h: 210, s: 50, l: 40 },
       { h: 210, s: 50, l: 40 },
@@ -31,6 +32,7 @@ class App extends React.Component {
             <InputBox />
             <ColourInfo lines={this.state.lines} />
           </div>
+          <ColourBlocks rgbs={this.state.rgbs} />
         </div>
       </div>
     );
