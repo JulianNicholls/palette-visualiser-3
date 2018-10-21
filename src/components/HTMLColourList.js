363a83eb469cd9394582fe6dcb7f18481f489c5b
Julian Nicholls <juliannicholls29@gmail.com>
2018-10-21 12:27:23 +0100

    Add displayt of XKCD colours

--- src/components/HTMLColourList.js
+++ src/components/HTMLColourList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import HTMLColours from '../HTMLColours';
+import XKCDColours from '../XKCDColours';
+
 import {
   ratioThreshold,
   rgbStrToObject,
@@ -13,12 +15,17 @@ const white = { r: 255, g: 255, b: 255 };
 
 class HTMLColourList extends React.Component {
   state = {
+    selected: 'html',
     colourList: [],
     sortOrder: 'name'
   };
 
   componentDidMount() {
-    const colours = [...HTMLColours];
+    this.loadColours();
+  }
+
+  loadColours = (selected = this.state.selected) => {
+    const colours = selected === 'xkcd' ? [...XKCDColours] : [...HTMLColours];
 
     colours.forEach(colour => {
       colour.rgb = rgbStrToObject(colour.value);
@@ -26,7 +33,7 @@ class HTMLColourList extends React.Component {
     });
 
     this.setState(() => ({ colourList: colours }));
-  }
+  };
 
   sortByName = () => {
     const { colourList, sortOrder } = this.state;
@@ -76,22 +83,43 @@ class HTMLColourList extends React.Component {
     this.setState(() => ({ colourList: colours, sortOrder: 'luminancer' }));
   };
 
+  swapColours = () => {
+    const newSelected = this.state.selected === 'xkcd' ? 'html' : 'xkcd';
+
+    this.loadColours(newSelected);
+
+    this.setState({ selected: newSelected });
+  };
+
   render() {
+    const { selected, colourList } = this.state;
+
     return (
       <div className="html-colour-list">
+        <span className="html-colour-list__title">
+          {selected.toUpperCase()} Colours
+        </span>
+        <button onClick={this.swapColours}>
+          Swap to {selected === 'xkcd' ? 'HTML' : 'XKCD'}
+        </button>
+
         <button onClick={this.sortByName}>Colour Name</button>
         <button onClick={this.sortByRGB}>RGB</button>
         <button onClick={this.sortByLuminance}>Luminance</button>
         <div>Black Contrast Ratio</div>
         <div>White Contrast Ratio</div>
 
-        {this.state.colourList.map(({ name, value, rgb, luminance }) => {
+        {colourList.map(({ name, value, rgb, luminance }) => {
           const blackRatio = contrastRatio(rgb, black);
           const whiteRatio = contrastRatio(rgb, white);
 
+          // Choose the colour for the name based on the better ratio
           const nameColour = blackRatio > whiteRatio ? 'black' : 'white';
-          const blackColour = blackRatio > ratioThreshold ? 'black' : '#7f7f7f';
-          const whiteColour = whiteRatio > ratioThreshold ? 'white' : '#7f7f7f';
+
+          // Display black and white on the colour, if the ratio is good then use
+          // black or white, otherwise use a mid-grey biased toward the opposite
+          const blackColour = blackRatio > ratioThreshold ? 'black' : '#a0a0a0';
+          const whiteColour = whiteRatio > ratioThreshold ? 'white' : '#606060';
 
           return [
             <div key={name} style={{ background: value, color: nameColour }}>
