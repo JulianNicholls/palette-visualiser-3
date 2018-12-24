7a49764c89e08ee2c07711bce0fd8f914849040e
Julian Nicholls <juliannicholls29@gmail.com>
2018-12-24 19:52:42 +0000

    Convert HSLInput and InputBox to functional

--- src/components/HSLInput.js
+++ src/components/HSLInput.js
@@ -1,50 +1,45 @@
 import React from 'react';
 
-class HSLInput extends React.Component {
-  handleChange = event => {
+const HSLInput = ({ colour, index, handleChangeHSL }) => {
+  const handleChange = event => {
     const { name, value } = event.target;
-    const { colour, index } = this.props;
 
     colour[name] = value;
 
-    this.props.handleChangeHSL(index, colour);
+    handleChangeHSL(index, colour);
   };
 
-  render() {
-    const { colour } = this.props;
-
-    return (
-      <div className="hsl-inputs">
-        <input
-          className="hsl-input"
-          type="number"
-          min="0"
-          max="360"
-          name="h"
-          value={colour.h}
-          onChange={this.handleChange}
-        />
-        <input
-          className="hsl-input"
-          type="number"
-          min="0"
-          max="100"
-          name="s"
-          value={colour.s}
-          onChange={this.handleChange}
-        />
-        <input
-          className="hsl-input"
-          type="number"
-          min="0"
-          max="100"
-          name="l"
-          value={colour.l}
-          onChange={this.handleChange}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="hsl-inputs">
+      <input
+        className="hsl-input"
+        type="number"
+        min="0"
+        max="360"
+        name="h"
+        value={colour.h}
+        onChange={handleChange}
+      />
+      <input
+        className="hsl-input"
+        type="number"
+        min="0"
+        max="100"
+        name="s"
+        value={colour.s}
+        onChange={handleChange}
+      />
+      <input
+        className="hsl-input"
+        type="number"
+        min="0"
+        max="100"
+        name="l"
+        value={colour.l}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
 
 export default HSLInput;
--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -7,58 +7,54 @@ import HSLInput from './HSLInput';
 
 const LABELS = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
 
-class InputBox extends React.Component {
-  renderSet(index, label, rgbs, hsls) {
-    return (
-      <React.Fragment key={index}>
-        <label htmlFor={`rgb-${index}`}>{label}</label>
-        <RGBInput
-          key={rgbs[index]}
-          index={index}
-          rgb={rgbs[index]}
-          handleChangeRGB={(index, value) =>
-            this.dispatch({ type: CHANGE_RGB, index, value })
-          }
-        />
-        <input
-          type="color"
-          className="swatch"
-          value={rgbs[index]}
-          onChange={event =>
-            this.dispatch({ type: CHANGE_RGB, index, value: event.target.value })
-          }
-        />
-        <HSLInput
-          index={index}
-          colour={hsls[index]}
-          handleChangeHSL={(index, value) =>
-            this.dispatch({ type: CHANGE_HSL, index, value })
-          }
-        />
-      </React.Fragment>
-    );
-  }
-
-  render() {
-    return (
-      <Consumer>
-        {({ rgbs, hsls, dispatch }) => {
-          this.dispatch = dispatch;
-
+const InputBox = () => {
+  return (
+    <Consumer>
+      {({ rgbs, hsls, dispatch }) => {
+        const renderSet = (index, label, rgbs, hsls) => {
           return (
-            <div id="input-box">
-              <span className="first">Colour</span>
-              <span>RGB</span>
-              <span>Picker</span>
-              <span>HSL</span>
-
-              {LABELS.map((label, idx) => this.renderSet(idx, label, rgbs, hsls))}
-            </div>
+            <React.Fragment key={index}>
+              <label htmlFor={`rgb-${index}`}>{label}</label>
+              <RGBInput
+                key={rgbs[index]}
+                index={index}
+                rgb={rgbs[index]}
+                handleChangeRGB={(index, value) =>
+                  dispatch({ type: CHANGE_RGB, index, value })
+                }
+              />
+              <input
+                type="color"
+                className="swatch"
+                value={rgbs[index]}
+                onChange={event =>
+                  dispatch({ type: CHANGE_RGB, index, value: event.target.value })
+                }
+              />
+              <HSLInput
+                index={index}
+                colour={hsls[index]}
+                handleChangeHSL={(index, value) =>
+                  dispatch({ type: CHANGE_HSL, index, value })
+                }
+              />
+            </React.Fragment>
           );
-        }}
-      </Consumer>
-    );
-  }
-}
+        };
+
+        return (
+          <div id="input-box">
+            <span className="first">Colour</span>
+            <span>RGB</span>
+            <span>Picker</span>
+            <span>HSL</span>
+
+            {LABELS.map((label, idx) => renderSet(idx, label, rgbs, hsls))}
+          </div>
+        );
+      }}
+    </Consumer>
+  );
+};
 
 export default InputBox;
