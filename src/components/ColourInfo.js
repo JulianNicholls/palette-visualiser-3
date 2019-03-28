4ba8a7a4f002d5d06ce2c9ffb6f252966c4d09b1
Julian Nicholls <juliannicholls29@gmail.com>
2019-03-28 21:26:01 +0000

    Re-factor ColourInfo

--- src/components/ColourInfo.js
+++ src/components/ColourInfo.js
@@ -1,37 +1,9 @@
 import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
+
+import ColourInfoLine from './ColourInfoLine';
 
 import { ColourContext } from '../context';
 import { rgbStrToObject, sRGBLuminance, RGBtoHSV } from '../conversions';
-
-const ColourInfoLine = ({ colour: { rgbs, luminance, hsvs }, index }) => (
-  <tr className={index % 2 ? 'odd' : 'even'}>
-    <td>{rgbs.r}</td>
-    <td>{rgbs.g}</td>
-    <td>{rgbs.b}</td>
-    <td className="luminance">{luminance}</td>
-    <td>{hsvs.h}&deg;</td>
-    <td>{hsvs.s}%</td>
-    <td>{hsvs.v}%</td>
-  </tr>
-);
-
-ColourInfoLine.propTypes = {
-  colour: PropTypes.shape({
-    rgbs: PropTypes.shape({
-      r: PropTypes.number.isRequired,
-      g: PropTypes.number.isRequired,
-      b: PropTypes.number.isRequired,
-    }).isRequired,
-    luminance: PropTypes.string.isRequired,
-    hsvs: PropTypes.shape({
-      h: PropTypes.number.isRequired,
-      s: PropTypes.number.isRequired,
-      v: PropTypes.number.isRequired,
-    }).isRequired,
-  }).isRequired,
-  index: PropTypes.number.isRequired,
-};
 
 const ColourInfo = () => {
   const { rgbs } = useContext(ColourContext);
