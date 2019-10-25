55e27744396924fb49e5e43860a2582943a0d7cb
Julian Nicholls <juliannicholls29@gmail.com>
2019-10-25 21:07:00 +0100

    Update to latest module versions

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
 
   const extractSass = new ExtractTextPlugin({
-    filename: '[name].css'
+    filename: '[name].css',
   });
 
   return {
@@ -14,41 +14,34 @@ module.exports = (env, argv) => {
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          use: { loader: 'babel-loader' }
+          use: { loader: 'babel-loader' },
         },
         {
           test: /\.html$/,
           use: [
             {
               loader: 'html-loader',
-              options: { minimize: true }
-            }
-          ]
+              options: { minimize: true },
+            },
+          ],
         },
         {
           test: /\.s?css$/,
           use: extractSass.extract({
-            use: [
-              {
-                loader: 'css-loader'
-              },
-              {
-                loader: 'sass-loader'
-              }
-            ],
-            fallback: 'style-loader'
-          })
-        }
-      ]
+            use: [{ loader: 'css-loader' }, { loader: 'sass-loader' }],
+            fallback: 'style-loader',
+          }),
+        },
+      ],
     },
     plugins: [
       new HtmlWebPackPlugin({
         template: './src/index.html',
-        filename: './index.html'
+        filename: './index.html',
       }),
-      extractSass
+      extractSass,
     ],
 
-    devtool: isProduction ? 'source-map' : 'inline-source-map'
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
   };
 };
